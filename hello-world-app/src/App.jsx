51e70f9d3bc68f1d223edc7b95ed5f6460edfe3e
Santho257 <santhoshkumar02553@gmail.com>
2024-12-11 18:29:40 +0530

    Created custom message

--- hello-world-app/src/App.jsx
+++ hello-world-app/src/App.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
+import './App.css';
 
 const App = () => {
-  const text = "HELLO WORLD!";
+  const [text, setText] = useState("HELLO WORLD!");
   const [display, setDisplay] = useState("");
+  const [customText, setCustomText] = useState("");
   const [count, setCount] = useState(0);
 
   useEffect(() => {
@@ -14,11 +16,23 @@ const App = () => {
     }, 1000);
 
     return () => clearTimeout(timeout);
-  }, [count])
+  }, [count]);
+
+  const changeText = () => {
+    if (customText.trim()) {
+      setDisplay("");
+      setText(customText.trim().toUpperCase());
+      setCount(0);
+    }
+  }
 
   return (
     <>
-      <h1>{display}</h1>
+      <h1 className="text-content">{display}<span className="dots">{display.length != text.length && "..."}</span></h1>
+      <h3>Change Text?</h3>
+      <input type="text" value={customText} onChange={e => setCustomText(e.target.value)} />
+      <button onClick={changeText}>Change Text</button>
+
     </>
   )
 }
