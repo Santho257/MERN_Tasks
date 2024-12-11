b32502baa8c2c9d0b12ca92e6c7d39937bb13833
Santho257 <santhoshkumar02553@gmail.com>
2024-12-11 09:09:06 +0530

    Network Request Simulation Promises Exercise

---
+++ JS Exercises/Advanced Concepts/Promises.js
@@ -0,0 +1,13 @@
+const networkRequest = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let statusCode = 200 + Math.floor(Math.random() * 400);
+        if(statusCode < 400){
+            resolve(`Request worked successfully with statusCode :: ${statusCode}`);
+        }
+        else{
+            reject(`Request failed with statusCode :: ${statusCode}`);
+        }
+    }, 2000);
+});
+
+const result = networkRequest.then(console.log, console.log);
