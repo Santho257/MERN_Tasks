ed1940716d84cc39902db698156a29e0a31e1f94
Santho257 <santhoshkumar02553@gmail.com>
2024-12-11 18:11:06 +0530

    Created Hello World Text comes one by one

--- hello-world-app/src/App.jsx
+++ hello-world-app/src/App.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
+  const text = "HELLO WORLD!";
+  const [display, setDisplay] = useState("");
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (count < text.length) {
+        setDisplay(display + text[count]);
+        setCount(count + 1);
+      }
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [count])
+
   return (
-    <></>
+    <>
+      <h1>{display}</h1>
+    </>
   )
 }
 
