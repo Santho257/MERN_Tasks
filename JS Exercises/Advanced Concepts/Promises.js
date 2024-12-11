eff1d94327a5668d6cde102a67054e50ef3cafba
Santho257 <santhoshkumar02553@gmail.com>
2024-12-11 09:11:35 +0530

    Pushed the code into a function

--- JS Exercises/Advanced Concepts/Promises.js
+++ JS Exercises/Advanced Concepts/Promises.js
@@ -1,13 +1,17 @@
-const networkRequest = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        let statusCode = 200 + Math.floor(Math.random() * 400);
-        if(statusCode < 400){
-            resolve(`Request worked successfully with statusCode :: ${statusCode}`);
-        }
-        else{
-            reject(`Request failed with statusCode :: ${statusCode}`);
-        }
-    }, 2000);
-});
+const requestSimulation = () => {
+    const networkRequest = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let statusCode = 200 + Math.floor(Math.random() * 400);
+            if(statusCode < 400){
+                resolve(`Request worked successfully with statusCode :: ${statusCode}`);
+            }
+            else{
+                reject(`Request failed with statusCode :: ${statusCode}`);
+            }
+        }, 2000);
+    });
+    
+    const result = networkRequest.then(console.log, console.log);
+}
 
-const result = networkRequest.then(console.log, console.log);
+requestSimulation();
