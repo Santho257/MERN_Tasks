25814a80ca6de782330dbffc8c057d9507b639fb
Santho257 <santhoshkumar02553@gmail.com>
2024-12-12 18:04:57 +0530

    Written a first route

--- Day4-SimpleServer/src/app.js
+++ Day4-SimpleServer/src/app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const {join} = require("path");
 
 const app = express();
+
+app.get("/", (req, res) => {
+    res.sendFile(join(__dirname, "../public", "static", "welcome.html"), (err) => {
+        res.send(err);
+    })
+})
 
 module.exports = app;
