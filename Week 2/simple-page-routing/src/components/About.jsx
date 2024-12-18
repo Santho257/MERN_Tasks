1ca7bd96d1da1ca9786e88dc1187aa9bbb431fc3
Santho257 <santhoshkumar02553@gmail.com>
2024-12-18 11:03:19 +0530

    Created About and Contact component

---
+++ Week 2/simple-page-routing/src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <>
+            <h1>About</h1>
+            <p>A poineer in software providing for all your buisness needs</p>
+        </>
+    )
+}
+
+export default About
---
+++ Week 2/simple-page-routing/src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <React.Fragment>
+        <h1>Contact Us</h1>
+        <a href='mailto:abc@gmail.com'>abc@gmail.com</a>
+    </React.Fragment>
+  )
+}
+
+export default Contact
