ade53bc6d115a8651d248685e3bdd394cafa8d21
Santho257 <santhoshkumar02553@gmail.com>
2024-12-18 15:25:43 +0530

    Created and tested a card component

--- Week 2/card-holder-app/src/App.jsx
+++ Week 2/card-holder-app/src/App.jsx
@@ -1,6 +1,12 @@
+import CardElement from "./components/CardElement";
+import imgSrc from "./assets/mindsets.png";
 const App = () => {
+    const title = "Mindsets of People";
+    const description = `Mindset of a person can be classified into three types based on their reaction to something: Aggressive, Passive & Neutral`
     return (
-        <></>
+        <>
+            <CardElement imgSrc={imgSrc} title={title} description={description} />
+        </>
     );
 }
 
