2ad827695bc3a3ce802091fb09c6b82e9a9e0f99
Santho257 <santhoshkumar02553@gmail.com>
2024-12-18 15:25:20 +0530

    Created a card component using inline CSS

--- Week 2/card-holder-app/src/components/CardElement.jsx
+++ Week 2/card-holder-app/src/components/CardElement.jsx
@@ -7,19 +7,30 @@ const CardElement = ({ imgSrc, title, description }) => {
         width: "30%",
         height: "300px",
         padding: "20px",
-        margin: "10px",
+        margin: "10px 1%",
         borderRadius: "10px",
+        border: "10px solid green",
+        alignItems: "center",
     }
     const imgStyles = {
-        width: "calc(80% - 40px)",
+        width: "calc(100% - 40px)",
         height: "30%",
         borderRadius: "10px"
     }
+    const titleStyles = {
+        margin: "20px 0",
+        padding: "5px",
+        color: "green",
+        border: "1px solid grey"
+    }
+    const descStyles = {
+        textAlign: "justify",
+    }
     return (
         <section style={cardStyles}>
-            <img src={imgSrc} alt='This is an image' />
-            <h3>{title}</h3>
-            <p>{description}</p>
+            <img style={imgStyles} src={imgSrc} alt='This is an image' />
+            <h3 style={titleStyles}>{title}</h3>
+            <p style={descStyles}>{description}</p>
         </section>
     )
 }
