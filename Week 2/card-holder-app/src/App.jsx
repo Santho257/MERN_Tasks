4a7474f124035c0b08bca9dab0c8029dfebd931a
Santho257 <santhoshkumar02553@gmail.com>
2024-12-18 18:09:11 +0530

    Tested Dynamic buttons

--- Week 2/card-holder-app/src/App.jsx
+++ Week 2/card-holder-app/src/App.jsx
@@ -1,6 +1,7 @@
 import CardElement, { Description, Img, Title } from "./components/CardElement";
 import imgSrc from "./assets/londonstreet.jpg";
 import FlexWrapper from "./components/CSS-In-JS/FlexWrapper";
+import DynamicButton from "./components/CSS-In-JS/DynamicButton";
 const App = () => {
     const title = "Mindsets of People";
     const description = `Mindset of a person can be classified into three types based on their reaction to something: Aggressive, Passive & Neutral`
@@ -12,22 +13,8 @@ const App = () => {
                     <Title>{title}</Title>
                     <Description>{description}</Description>
                 </CardElement>
-                <CardElement>
-                    <Img src={imgSrc} />
-                    <Title>{title}</Title>
-                    <Description>{description}</Description>
-                </CardElement>
-                <CardElement>
-                    <Img src={imgSrc} />
-                    <Title>{title}</Title>
-                    <Description>{description}</Description>
-                </CardElement>
-                <CardElement>
-                    <Img src={imgSrc} />
-                    <Title>{title}</Title>
-                    <Description>{description}</Description>
-                </CardElement>
             </FlexWrapper>
+            <DynamicButton variant={"primary"} size={"xs"} styles={{border: "2px solid purple"}} className={"dummy"}>Click</DynamicButton>
         </>
     );
 }
