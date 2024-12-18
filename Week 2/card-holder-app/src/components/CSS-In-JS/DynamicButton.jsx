e94d2c841d70a31316907fefd0ee30fef76730f0
Santho257 <santhoshkumar02553@gmail.com>
2024-12-18 22:27:12 +0530

    Changed the styles

--- Week 2/card-holder-app/src/components/CSS-In-JS/DynamicButton.jsx
+++ Week 2/card-holder-app/src/components/CSS-In-JS/DynamicButton.jsx
@@ -4,11 +4,9 @@ import { variants } from "../../utilities/variantUtility";
 const DynamicButton = ({ children, size, variant, styles, ...rest }) => {
     const btnStyles = {
         cursor: "pointer",
-        margin: "auto 0.25rem",
         border: "none",
         ...styles,
-        backgroundColor: variants[variant]?.backgroundColor ?? "white",
-        color: variants[variant]?.color ?? "black",
+        ...variants[variant],
         ...sizes.button[size]
     }
     return <button {...rest} style={btnStyles}>{children}</button>;
