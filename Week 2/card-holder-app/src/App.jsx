0ef9954d2b17b68ef8062014b2aa40393eb95441
Santho257 <santhoshkumar02553@gmail.com>
2024-12-18 22:28:38 +0530

    Rendered CreateButton component

--- Week 2/card-holder-app/src/App.jsx
+++ Week 2/card-holder-app/src/App.jsx
@@ -2,19 +2,11 @@ import CardElement, { Description, Img, Title } from "./components/CardElement";
 import imgSrc from "./assets/londonstreet.jpg";
 import FlexWrapper from "./components/CSS-In-JS/FlexWrapper";
 import DynamicButton from "./components/CSS-In-JS/DynamicButton";
+import CreateButton from "./components/CreateButton";
 const App = () => {
-    const title = "Mindsets of People";
-    const description = `Mindset of a person can be classified into three types based on their reaction to something: Aggressive, Passive & Neutral`
     return (
         <>
-            <FlexWrapper>
-                <CardElement>
-                    <Img src={imgSrc} />
-                    <Title>{title}</Title>
-                    <Description>{description}</Description>
-                </CardElement>
-            </FlexWrapper>
-            <DynamicButton variant={"primary"} size={"xs"} styles={{border: "2px solid purple"}} className={"dummy"}>Click</DynamicButton>
+            <CreateButton />
         </>
     );
 }
