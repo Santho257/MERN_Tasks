b05b30488480bf208efd4d13555d7eaf59312c04
Santho257 <santhoshkumar02553@gmail.com>
2024-12-18 16:51:00 +0530

    Converted CardElement to CSS-in-JS stylings

--- Week 2/card-holder-app/src/App.jsx
+++ Week 2/card-holder-app/src/App.jsx
@@ -1,11 +1,33 @@
-import CardElement from "./components/CardElement";
-import imgSrc from "./assets/mindsets.png";
+import CardElement, { Description, Img, Title } from "./components/CardElement";
+import imgSrc from "./assets/londonstreet.jpg";
+import FlexWrapper from "./components/CSS-In-JS/FlexWrapper";
 const App = () => {
     const title = "Mindsets of People";
     const description = `Mindset of a person can be classified into three types based on their reaction to something: Aggressive, Passive & Neutral`
     return (
         <>
-            <CardElement imgSrc={imgSrc} title={title} description={description} />
+            <FlexWrapper>
+                <CardElement>
+                    <Img src={imgSrc} />
+                    <Title>{title}</Title>
+                    <Description>{description}</Description>
+                </CardElement>
+                <CardElement>
+                    <Img src={imgSrc} />
+                    <Title>{title}</Title>
+                    <Description>{description}</Description>
+                </CardElement>
+                <CardElement>
+                    <Img src={imgSrc} />
+                    <Title>{title}</Title>
+                    <Description>{description}</Description>
+                </CardElement>
+                <CardElement>
+                    <Img src={imgSrc} />
+                    <Title>{title}</Title>
+                    <Description>{description}</Description>
+                </CardElement>
+            </FlexWrapper>
         </>
     );
 }
--- Week 2/card-holder-app/src/components/CardElement.jsx
+++ Week 2/card-holder-app/src/components/CardElement.jsx
@@ -1,13 +1,49 @@
-import cardStyles from './CardElement.module.css'
-
-const CardElement = ({ imgSrc, title, description }) => {
+const CardElement = ({ children, ...rest }) => {
+    const cardStyles = {
+        display: "flex",
+        flexDirection: "column",
+        width: "30%",
+        height: "300px",
+        padding: "20px",
+        margin: "10px 1%",
+        borderRadius: "20px",
+        border: "1px solid #8f94fb",
+        alignItems: "center",
+        ...rest.styles
+    }
     return (
-        <section className={cardStyles.card}>
-            <img src={imgSrc} alt='This is an image' className={cardStyles.cardImg}/>
-            <h3 className={cardStyles.cardTitle}>{title}</h3>
-            <p className={cardStyles.cardDescription}>{description}</p>
+        <section style={cardStyles}>
+            {children}
         </section>
     )
+}
+export const Img = ({children, src, ...rest}) => {
+    const imgStyles = {
+        width: "calc(100% - 40px)",
+        height: "50%",
+        borderRadius: "10px",
+        ...rest.styles
+    }
+    return <img style={imgStyles} src={src} alt={rest.alt ?? ""} />
+}
+
+export const Title = ({children, ...rest}) => {
+    const titleStyles = {
+        margin: "20px 0",
+        padding: "5px",
+        color: "#4e54c8",
+        border: "1px solid grey",
+        ...rest.styles
+    }
+    return <h3 style={titleStyles}>{children}</h3>
+}
+
+export const Description = ({children, ...rest}) => {
+    const descStyles = {
+        textAlign: "justify",
+        ...rest.styles
+    }
+    return <p style={descStyles}>{children}</p>;
 }
 
 export default CardElement
