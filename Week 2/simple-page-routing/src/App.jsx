251c493c2757cee227d003dd4f46a33751c95002
Santho257 <santhoshkumar02553@gmail.com>
2024-12-18 11:03:54 +0530

    Written Links to the components

--- Week 2/simple-page-routing/src/App.jsx
+++ Week 2/simple-page-routing/src/App.jsx
@@ -1,9 +1,24 @@
-import { Route, Routes } from "react-router-dom"
+import { Link, Route, Routes } from "react-router-dom"
 import Home from "./components/Home"
 
 const App = () => {
   return (
     <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <Routes>
         <Route path="/" element={<Home />} /> 
       </Routes>
