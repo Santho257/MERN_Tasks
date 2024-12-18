1d2ab1da860f2487d02f82211be090d3ce19ff68
Santho257 <santhoshkumar02553@gmail.com>
2024-12-18 18:08:17 +0530

    Written Size and Variant utilities

---
+++ Week 2/card-holder-app/src/utilities/variantUtility.js
@@ -0,0 +1,18 @@
+export const variants = {
+    primary: {
+        backgroundColor: "blue",
+        color: "white"
+    },
+    dark: {
+        backgroundColor: "black",
+        color: "white"
+    },
+    success: {
+        backgroundColor: "green",
+        color: "white"
+    },
+    danger: {
+        backgroundColor: "red",
+        color: "white"
+    }
+};
