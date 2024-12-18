8a9c4fe776a9412e4c2362cae6df348351b20d13
Santho257 <santhoshkumar02553@gmail.com>
2024-12-18 15:46:06 +0530

    Separated Inline CSS to CSS modules

--- Week 2/card-holder-app/src/components/CardElement.jsx
+++ Week 2/card-holder-app/src/components/CardElement.jsx
@@ -1,36 +1,11 @@
-import React from 'react'
+import cardStyles from './CardElement.module.css'
 
 const CardElement = ({ imgSrc, title, description }) => {
-    const cardStyles = {
-        display: "flex",
-        flexDirection: "column",
-        width: "30%",
-        height: "300px",
-        padding: "20px",
-        margin: "10px 1%",
-        borderRadius: "10px",
-        border: "10px solid green",
-        alignItems: "center",
-    }
-    const imgStyles = {
-        width: "calc(100% - 40px)",
-        height: "30%",
-        borderRadius: "10px"
-    }
-    const titleStyles = {
-        margin: "20px 0",
-        padding: "5px",
-        color: "green",
-        border: "1px solid grey"
-    }
-    const descStyles = {
-        textAlign: "justify",
-    }
     return (
-        <section style={cardStyles}>
-            <img style={imgStyles} src={imgSrc} alt='This is an image' />
-            <h3 style={titleStyles}>{title}</h3>
-            <p style={descStyles}>{description}</p>
+        <section className={cardStyles.card}>
+            <img src={imgSrc} alt='This is an image' className={cardStyles.cardImg}/>
+            <h3 className={cardStyles.cardTitle}>{title}</h3>
+            <p className={cardStyles.cardDescription}>{description}</p>
         </section>
     )
 }
