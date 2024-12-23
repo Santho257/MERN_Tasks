17a25fc67be230d1ec383478fcedf5663aef03f4
Santho257 <santhoshkumar02553@gmail.com>
2024-12-23 13:39:04 +0530

    Used the UI components

--- Week 3/GlobalTheme/src/App.jsx
+++ Week 3/GlobalTheme/src/App.jsx
@@ -1,8 +1,24 @@
+import Link from "./components/ui/Link/Link"
+import Section from "./components/ui/Section/Section"
+import ThemeSetter from "./components/ThemeSetter/ThemeSetter"
+import Button from "./components/ui/Button/Button"
+
 const App = () => {
-  return (
-    <>
-    </>
-  )
+	return (
+		<>
+			<ThemeSetter />
+			<Section>
+				<Section>
+					<h1>Hello Everyone</h1>
+					<Section>
+						<p>In this passage, we are gonna "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."</p>
+						<p><Link href="https://www.lipsum.com/">To Learn more</Link></p>
+					</Section>
+					<Button>Click Me</Button>
+				</Section>
+			</Section>
+		</>
+	)
 }
 
 export default App
--- Week 3/GlobalTheme/src/components/ThemeSetter/ThemeSetter.jsx
+++ Week 3/GlobalTheme/src/components/ThemeSetter/ThemeSetter.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { ThemeContext } from '../../contexts/ThemeContext';
 import styles from './ThemeSetter.module.css';
+import Section from '../ui/Section/Section';
 
 const ThemeSetter = () => {
     const {darkTheme, toggleDarkTheme} = useContext(ThemeContext);
@@ -8,9 +9,9 @@ const ThemeSetter = () => {
         backgroundImage: `url(${darkTheme ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnIOipnArAC68qN_3YIkiPV1bd79fPfKSh2w&s" : "https://media.istockphoto.com/id/1324413691/photo/beautiful-sky-with-white-clouds.jpg?s=612x612&w=0&k=20&c=40a8JIoRJ1BAI4XeyEouZl0QbVAJToDBRQn31S3FGzk="})`
     }
     return (
-        <section className={styles.holder}>
+        <Section className={styles.holder}>
             <input style={buttonStyles} className={styles.checkbox} type="checkbox" name="dark" id="dark" checked={darkTheme} onChange={e => toggleDarkTheme(!e.target.checked)} />
-        </section>
+        </Section>
     )
 }
 
