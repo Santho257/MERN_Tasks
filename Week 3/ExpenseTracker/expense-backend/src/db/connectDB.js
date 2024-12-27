b571aca2506fbd991e5273a09bc0fd13d1a16992
Santho257 <santhoshkumar02553@gmail.com>
2024-12-27 12:27:45 +0530

    Expense tracker: Converted from promise to async await

--- Week 3/ExpenseTracker/expense-backend/src/db/connectDB.js
+++ Week 3/ExpenseTracker/expense-backend/src/db/connectDB.js
@@ -2,14 +2,14 @@ import { connect } from 'mongoose'
 import { connectionString, database } from '../constants.js'
 
 const connectDB = async () => {
-    connect(`${connectionString}/${database}`)
-        .then((instance) => {
-            console.log(`Connected to Database on :: ${instance.connection.host}`);
-        })
-        .catch((err) => {
-            console.error(`Error connecting to database`);
-            console.error(`${connectionString}/${database}`);
-        });
+    try {
+        const instance = await connect(`${connectionString}/${database}`)
+        console.log(`Connected to Database on :: ${instance.connection.host}`);
+    } catch (error) {
+        console.error(`Error connecting to database`);
+        console.error(`${connectionString}/${database}`);
+        process.exit(1);
+    }
 }
 
 export {connectDB};
