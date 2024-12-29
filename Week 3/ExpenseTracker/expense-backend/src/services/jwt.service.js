fed8de91fa2093acbf967740135d68f8f1241647
Santho257 <santhoshkumar02553@gmail.com>
2024-12-29 16:37:26 +0530

    Expense Tracker: Created JWT Service

---
+++ Week 3/ExpenseTracker/expense-backend/src/services/jwt.service.js
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken';
+import { jwtSecret } from '../constants.js';
+
+export const generateToken = (payload) => {
+    const token = jwt.sign(payload, jwtSecret, {expiresIn: '1d'});
+    return token;
+}
+
+export const verifyToken = (token) => {
+    return jwt.verify(token, jwtSecret);
+}
