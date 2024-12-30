e34bddb2fcc31208a049287956b11987a6122b08
Santho257 <santhoshkumar02553@gmail.com>
2024-12-30 11:27:43 +0530

    Expense Tracker: Created authentication middleware

---
+++ Week 3/ExpenseTracker/expense-backend/src/middlewares/requireAuth.middleware.js
@@ -0,0 +1,15 @@
+import { verifyToken } from "../services/jwt.service.js";
+import ApiError from "../utils/ApiError.js";
+import { AsyncHandler } from "../utils/AsyncHandler.js";
+
+AsyncHandler(async (req, res, next) => {
+    try{
+        const token = req.headers.authorization;
+        if(!token)  throw new ApiError("Token not available", 401, {token: "Token Invalid"});
+        else if(verifyToken(token))  next();
+        else    throw new ApiError("Token invalid", 401, {token: "Token Invalid"});
+    }
+    catch(error){
+        throw error;
+    }
+});
