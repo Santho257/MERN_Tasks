5f6ada2f9d08530d6f3347abe16579aee2afb224
Santho257 <santhoshkumar02553@gmail.com>
2024-12-30 11:42:51 +0530

    Expense Tracker: Protecting routes on front end

---
+++ Week 3/ExpenseTracker/expense-frontend/src/components/Authentication/RequireAuth.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from "react-router-dom";
+import React, { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+
+const RequireAuth = () => {
+    const auth = useContext(AuthContext);
+    if(auth.user.id == ''){
+        return <Navigate to="/login" replace/>
+    }
+    return <Outlet/>;
+}
+
+export default React.memo(RequireAuth);
