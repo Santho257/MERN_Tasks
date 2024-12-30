e9c79dbd9492b00f82ce25d09a5de4c512733ef1
Santho257 <santhoshkumar02553@gmail.com>
2024-12-30 11:32:45 +0530

    Expense Tracker: Updated Error Message

--- Week 3/ExpenseTracker/expense-backend/src/middlewares/requireAuth.middleware.js
+++ Week 3/ExpenseTracker/expense-backend/src/middlewares/requireAuth.middleware.js
@@ -5,7 +5,7 @@ import { AsyncHandler } from "../utils/AsyncHandler.js";
 export const requireAuth = AsyncHandler(async (req, res, next) => {
     try{
         const token = req.headers.authorization;
-        if(!token)  throw new ApiError("Token not available", 401, {token: "Token Invalid"});
+        if(!token)  throw new ApiError("Token not found", 401, {token: "Token not found"});
         else if(verifyToken(token))  next();
         else    throw new ApiError("Token invalid", 401, {token: "Token Invalid"});
     }
