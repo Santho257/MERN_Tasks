585e96452510f4bcb44f9c5a90e90176cc06fef5
Santho257 <santhoshkumar02553@gmail.com>
2024-12-31 14:36:00 +0530

    Expense Tracker: Removed Input module css and written in CSS-in-JS format

--- Week 3/ExpenseTracker/expense-frontend/src/ui/Input/Input.jsx
+++ Week 3/ExpenseTracker/expense-frontend/src/ui/Input/Input.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react'
 import { ThemeContext } from '../../contexts/ThemeContext'
 import Section from '../Section/Section';
-import modStyles from './Input.module.css'
 
 const Input = ({ style, error, ...rest }) => {
     const { darkTheme } = useContext(ThemeContext);
@@ -12,17 +11,32 @@ const Input = ({ style, error, ...rest }) => {
         margin: "0 5%",
         borderRadius: "1rem",
         padding: "1rem",
+        border: "1px solid",
         ...style,
         backgroundColor: darkTheme ? "#000000" : "#ffffff",
         color: darkTheme ? "#fff" : "#232323",
         outline: "none",
-        border: darkTheme ? "none" : "1px solid #cdcef0"
+        borderColor: error ? "red" : darkTheme ? "transparent" : "#cdcef0",
+        
+    }
+    const labelStyles = {
+        position: "absolute",
+        fontSize: "0.75rem",
+        top: "0.5rem",
+        left: "3rem",
+        zIndex: 1,
+        padding: "0 0.25rem",
+        backgroundColor: darkTheme ? "#232323" : "#fff"
+    }
+    const errStyles= {
+        color: "red",
+        margin: "0 5%",
     }
     return (
         <Section style={{ position: "relative" }}>
-            {(rest.placeholder && !rest.value) || <label htmlFor={rest.id}>{rest.placeholder || rest.name}</label>}
+            {(rest.placeholder && !rest.value) || <label style={labelStyles} htmlFor={rest.id}>{rest.placeholder || rest.name}</label>}
             <input style={styles} {...rest} />
-            <span className={modStyles.error}>{error}</span>
+            <span style={errStyles}>{error}</span>
         </Section>
     )
 }
