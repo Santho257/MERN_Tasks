1c4a6703051978fa9dc66af46c9a9a5efdcab132
Santho257 <santhoshkumar02553@gmail.com>
2024-12-31 14:30:09 +0530

    Expense Tracker: Added get by ID method

--- Week 3/ExpenseTracker/expense-backend/src/routes/expense.route.js
+++ Week 3/ExpenseTracker/expense-backend/src/routes/expense.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { addExpense, deleteExpense, getExpenses, updateExpense } from "../controllers/expense.controller.js";
+import { addExpense, deleteExpense, getExpenseById, getExpenses, updateExpense } from "../controllers/expense.controller.js";
 
 const ExpenseRouter = Router();
 ExpenseRouter.route("").post(addExpense);
+ExpenseRouter.route("/:id").get(getExpenseById);
 ExpenseRouter.route("/:id").patch(updateExpense);
 ExpenseRouter.route("/:id").delete(deleteExpense);
 ExpenseRouter.route("/explists/:expenseList").get(getExpenses);
--- Week 3/ExpenseTracker/expense-backend/src/routes/explist.route.js
+++ Week 3/ExpenseTracker/expense-backend/src/routes/explist.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createExpenseList, deleteList, getExpenseLists, updateList } from "../controllers/explist.controller.js";
+import { createExpenseList, deleteList, getExpenseLists, getListById, updateList } from "../controllers/explist.controller.js";
 
 const ExpListRouter = Router();
 
 ExpListRouter.route("").post(createExpenseList);
 ExpListRouter.route("").get(getExpenseLists);
+ExpListRouter.route("/:id").get(getListById);
 ExpListRouter.route("/:id").patch(updateList);
 ExpListRouter.route("/:id").delete(deleteList);
 
