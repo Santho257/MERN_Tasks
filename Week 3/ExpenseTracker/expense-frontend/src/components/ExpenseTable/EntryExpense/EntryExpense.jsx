002ad01699bc2354c34e3917d793358a2be6290d
Santho257 <santhoshkumar02553@gmail.com>
2024-12-31 17:41:15 +0530

    Expense Tracker: Created expense components

---
+++ Week 3/ExpenseTracker/expense-frontend/src/components/ExpenseTable/EntryExpense/EntryExpense.jsx
@@ -0,0 +1,117 @@
+import React, { useContext, useState } from "react";
+import InputNS from "../../../ui/Input/InputNS";
+import { Options } from "../../../ui/Select/Select";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../../../constants";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { TD } from "../../../ui/Table/Table";
+import SelectNS from "../../../ui/Select/SelectNS";
+
+const EntryExpense = ({ setCount }) => {
+  const { explistId } = useParams();
+  const { user } = useContext(AuthContext);
+  const [formdata, setFormdata] = useState({
+    date: "",
+    category: "",
+    source: "",
+    amount: "",
+  });
+
+  const [errors, setErrors] = useState({
+    source: "",
+    amount: "",
+    date: "",
+    category: "",
+  });
+  const handleFieldChange = (field, value) => {
+    setFormdata({ ...formdata, [field]: value });
+  };
+
+  const entryExpense = async () => {
+    try {
+      const result = await axios.post(
+        `${BASE_URL}/expenses`,
+        { ...formdata, expenseList: explistId },
+        { headers: { Authorization: user.token } }
+      );
+      if (result.data.success) setCount((prev) => prev + 1);
+      setFormdata({
+        date: "",
+        category: "",
+        source: "",
+        amount: 0,
+      });
+      setCount((prev) => prev + 1);
+    } catch (error) {
+      console.log(error);
+      setErrors(error?.response?.data?.errors || { message: error.message });
+    }
+  };
+
+  return (
+    <>
+      <TD>
+        <InputNS
+          style={{ borderRadius: 0 }}
+          type="text"
+          name="source"
+          id="source"
+          value={formdata.source}
+          onChange={(e) => {
+            handleFieldChange("source", e.target.value);
+          }}
+          error={errors.source}
+        />
+      </TD>
+      <TD>
+        <SelectNS
+          name="category"
+          id="category"
+          onChange={(e) => handleFieldChange("category", e.target.value)}
+          value={formdata.category}
+          error={errors.category}
+        >
+          <Options
+            options={{
+              Category: "",
+              Food: "food",
+              Travel: "travel",
+              Personal: "personal",
+              Family: "family",
+            }}
+          />
+        </SelectNS>
+      </TD>
+      <TD>
+        <InputNS
+          type="date"
+          name="date"
+          id="date"
+          value={formdata.date}
+          onChange={(e) => {
+            handleFieldChange("date", e.target.value);
+          }}
+          error={errors.date}
+        />
+      </TD>
+      <TD>
+        <InputNS
+          type="number"
+          name="amount"
+          id="amount"
+          value={formdata.amount}
+          onChange={(e) => {
+            handleFieldChange("amount", e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key == "Enter") entryExpense();
+          }}
+          error={errors.amount}
+        />
+      </TD>
+    </>
+  );
+};
+
+export default React.memo(EntryExpense);
---
+++ Week 3/ExpenseTracker/expense-frontend/src/components/ExpenseTable/ExpenseTable.jsx
@@ -0,0 +1,76 @@
+import React, { useContext, useEffect, useMemo, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { BASE_URL } from "../../constants";
+import { Outlet, useParams } from "react-router-dom";
+import ExpenseData from "./ExpenseData/ExpenseData";
+import Section from "../../ui/Section/Section";
+import EntryExpense from "./EntryExpense/EntryExpense";
+import { useFetch } from "../../hooks/useFetch";
+import Title from "../../ui/Title/Title";
+import Table, { TD, TH } from "../../ui/Table/Table";
+
+const ExpenseTable = () => {
+  const { user } = useContext(AuthContext);
+  const { explistId } = useParams();
+  const [count, setCount] = useState(0);
+
+  const [expenses, , errors1] = useFetch(
+    `${BASE_URL}/expenses/explists/${explistId}`,
+    { Authorization: user.token },
+    [],
+    [count]
+  );
+
+  const [listData, , errors2] = useFetch(
+    `${BASE_URL}/explists/${explistId}`,
+    { Authorization: user.token },
+    {},
+    [count]
+  );
+
+  const findTotalExpense = useMemo(() => {
+    console.log("Calculating");
+    const total = expenses.reduce(
+      (total, expense) => parseInt(total) + parseInt(expense.amount),
+      0
+    );
+    return total;
+  }, [expenses]);
+
+  return (
+    <Section>
+      <Title>{listData?.title}</Title>
+      <Table>
+        <thead>
+          <tr>
+            <TH>S.No</TH>
+            <TH>Source</TH>
+            <TH>Category</TH>
+            <TH>Date</TH>
+            <TH>Amount</TH>
+          </tr>
+        </thead>
+        <tbody>
+          <ExpenseData expenses={expenses} />
+          <tr>
+            <TD>{expenses.length + 1}</TD>
+            <EntryExpense setCount={setCount} />
+          </tr>
+          <tr>
+            <TD colSpan={4} style={{ textAlign: "right" }}>Total</TD>
+            <TD>
+              {findTotalExpense.toLocaleString("en-IN", {
+                style: "currency",
+                currency: "INR",
+                currencyDisplay: "symbol",
+              })}
+            </TD>
+          </tr>
+        </tbody>
+      </Table>
+      <Outlet />
+    </Section>
+  );
+};
+
+export default ExpenseTable;
