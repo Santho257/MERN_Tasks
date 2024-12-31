0b824f8d0e635c5444017d46dc3e9a1e1e3a8696
Santho257 <santhoshkumar02553@gmail.com>
2024-12-31 18:34:19 +0530

    Expense Tracker: Added error with Input component

--- Week 3/ExpenseTracker/expense-frontend/src/components/ExpenseListArea/Create/Create.jsx
+++ Week 3/ExpenseTracker/expense-frontend/src/components/ExpenseListArea/Create/Create.jsx
@@ -1,33 +1,39 @@
-import { useContext, useState } from 'react'
-import Button from '../../../ui/Button/Button'
-import Input from '../../../ui/Input/Input'
-import AreaStyles from '../ExpenseListArea.module.css'
-import axios from 'axios'
-import { BASE_URL } from '../../../constants'
-import { AuthContext } from '../../../contexts/AuthContext'
+import { useContext, useState } from "react";
+import Button from "../../../ui/Button/Button";
+import Input from "../../../ui/Input/Input";
+import AreaStyles from "../ExpenseListArea.module.css";
+import axios from "axios";
+import { BASE_URL } from "../../../constants";
+import { AuthContext } from "../../../contexts/AuthContext";
+import InputNS from "../../../ui/Input/InputNS";
 const Create = ({ setCount }) => {
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
     const [errors, setErrors] = useState({});
-    const [title, setTitle] = useState("")
+    const [title, setTitle] = useState("");
     const createExpen = async (e) => {
         e.preventDefault();
         setErrors({});
         try {
-            const result = await axios.post(`${BASE_URL}/explists`, { title }, { headers:{Authorization: user.token}});
-            console.log(result.data.message)
-            setCount(prev => prev + 1);
+            const result = await axios.post(
+                `${BASE_URL}/explists`,
+                { title },
+                { headers: { Authorization: user.token } }
+            );
+            setCount((prev) => prev + 1);
         } catch (error) {
-            console.log(error);
-            setErrors(error.response.data.errors)
+            setErrors(error.response.data.errors);
         }
     };
     return (
         <form className={AreaStyles.form} onSubmit={createExpen}>
-            <Input value={title} onChange={(e) => setTitle(e.target.value)}/>
-            <sub className={AreaStyles.errors}>{errors.title}</sub>
+            <InputNS
+                value={title}
+                error={errors.title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
             <Button type="submit">Create</Button>
         </form>
-    )
-}
+    );
+};
 
-export default Create
+export default Create;
