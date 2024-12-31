aa8a6b4bf5098361c07eeb75400496a9ead293a0
Santho257 <santhoshkumar02553@gmail.com>
2024-12-31 17:42:07 +0530

    Expense Tracker: Make it to exact path so no other path matches

--- Week 3/ExpenseTracker/expense-frontend/src/components/Routes/Router.jsx
+++ Week 3/ExpenseTracker/expense-frontend/src/components/Routes/Router.jsx
@@ -3,14 +3,18 @@ import { Route, Routes } from 'react-router-dom'
 import Login from '../Login/Login'
 import RequireAuth from '../Authentication/RequireAuth'
 import ExpenseListArea from '../ExpenseListArea/ExpenseListArea'
+import ExpenseTable from '../ExpenseTable/ExpenseTable'
+import EditExpense from '../ExpenseTable/EditExpense/EditExpense'
 
 const Router = () => {
     return (
         <Routes>
-            <Route path='/login' element={<Login login/>}/>
-            <Route path='/signup' element={<Login/>}/>
+            <Route exact path='/login' element={<Login login/>}/>
+            <Route exact path='/signup' element={<Login/>}/>
             <Route element={<RequireAuth />}>
-                <Route path='/expenses' element={<ExpenseListArea />}/>
+                <Route exact path='/expenses' element={<ExpenseListArea />}/>
+                <Route exact path='/expenses/:explistId' element={<ExpenseTable />}/>
+                <Route exact path='/expenses/:explistId/:expId' element={<EditExpense />}/>
             </Route>
         </Routes>
     )
