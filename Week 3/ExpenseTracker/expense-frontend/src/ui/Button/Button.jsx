97c9f3e5abdb89da4fd9e214bc1c2545bf032219
Santho257 <santhoshkumar02553@gmail.com>
2025-01-02 13:20:37 +0530

    Expense Tracker: Changed styles

--- Week 3/ExpenseTracker/expense-frontend/src/ui/Button/Button.jsx
+++ Week 3/ExpenseTracker/expense-frontend/src/ui/Button/Button.jsx
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../../contexts/ThemeContext';
 
-const Button = ({ children, ...rest }) => {
+const Button = ({ children, style, ...rest }) => {
     const { darkTheme } = useContext(ThemeContext);
 
     const styles = {
         backgroundColor: darkTheme ? "#58c4dc" : "#2f7ea5",
         color: darkTheme ? "#2b5669" : "#effefe",
-        padding: "0.5rem",
+        padding: "0.5rem 1rem",
         border: "none",
         borderRadius: "1rem",
-        cursor: "pointer"
+        cursor: "pointer",
+        margin: "0 1rem",
+        ...style
     }
     return (
         <button style={styles} {...rest}>{children}</button>
