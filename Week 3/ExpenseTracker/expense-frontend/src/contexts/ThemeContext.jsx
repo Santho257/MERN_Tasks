0c19d31eb680c40660aff0863366fe0356c89c30
Santho257 <santhoshkumar02553@gmail.com>
2025-01-02 13:21:11 +0530

    Expemse Tracker: Stored theme change in localstorage to not lose access on refresh

--- Week 3/ExpenseTracker/expense-frontend/src/contexts/ThemeContext.jsx
+++ Week 3/ExpenseTracker/expense-frontend/src/contexts/ThemeContext.jsx
@@ -5,14 +5,15 @@ import { BASE_URL } from "../constants";
 
 const ThemeContext = createContext(false);
 
-const ThemeContextProvider = ({children}) => {
-    const {user} = useContext(AuthContext);
-    const [darkTheme, setDarkTheme] = useState(false);
+const ThemeContextProvider = ({ children }) => {
+    const { user } = useContext(AuthContext);
+    const [darkTheme, setDarkTheme] = useState(JSON.parse(localStorage.getItem("theme"))?.theme || false);
     const toggleDarkTheme = useCallback(async () => {
-        user.token && await axios.patch(`${BASE_URL}/users`, {theme: darkTheme ? "light" : "dark"},{headers: {Authorization: user.token}});
+        user.token && await axios.patch(`${BASE_URL}/users`, { theme: darkTheme ? "light" : "dark" }, { headers: { Authorization: user.token } });
+        localStorage.setItem("theme", JSON.stringify({ theme: !darkTheme }));
         setDarkTheme(!darkTheme);
     })
-    return <ThemeContext.Provider value={{darkTheme, toggleDarkTheme}}>{children}</ThemeContext.Provider>
+    return <ThemeContext.Provider value={{ darkTheme, toggleDarkTheme }}>{children}</ThemeContext.Provider>
 }
 
-export {ThemeContext, ThemeContextProvider};
+export { ThemeContext, ThemeContextProvider };
