2b8209adbc17ee4a36030a1e0907e77fb04c53b6
Santho257 <santhoshkumar02553@gmail.com>
2025-01-02 13:09:27 +0530

    Expense Tracker: Added a new section name, and send it with token

--- Week 3/ExpenseTracker/expense-backend/src/controllers/auth.controller.js
+++ Week 3/ExpenseTracker/expense-backend/src/controllers/auth.controller.js
@@ -4,28 +4,26 @@ import ApiResponse from "../utils/ApiResponse.js";
 import { AsyncHandler } from "../utils/AsyncHandler.js";
 
 const signUp = AsyncHandler(async (req, res) => {
-    try {
-        const newUser = await User.create(req.body);
-        const token = generateToken({id: newUser._id});
-        res.status(201).send(new ApiResponse(201, "User Created", {token}));
-    } catch (error) {
-        throw error;
-    }
+  try {
+    const newUser = await User.create(req.body);
+    const token = generateToken({ id: newUser._id, sub: newUser.name });
+    res.status(201).send(new ApiResponse(201, "User Created", { token }));
+  } catch (error) {
+    throw error;
+  }
 });
 
 const login = AsyncHandler(async (req, res) => {
-    const {email, password} = req.body;
-    try{
-        const user = await User.login(email, password);
-        const token = generateToken({id: user._id});
-        res.status(200).send(new ApiResponse(201, "Logged In successfully", {token}));
-    }
-    catch(error){
-        throw error;
-    }
-})
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = generateToken({ id: user._id, sub: user.name });
+    res
+      .status(200)
+      .send(new ApiResponse(201, "Logged In successfully", { token }));
+  } catch (error) {
+    throw error;
+  }
+});
 
-
-export {
-    signUp, login
-}
+export { signUp, login };
--- Week 3/ExpenseTracker/expense-backend/src/models/users.model.js
+++ Week 3/ExpenseTracker/expense-backend/src/models/users.model.js
@@ -2,57 +2,73 @@ import { model, Schema } from "mongoose";
 import { compare, genSaltSync, hashSync } from "bcrypt";
 import ApiError from "../utils/ApiError.js";
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     email: {
-        type: String,
-        unique: true,
-        index: true,
-        lowercase: true,
-        validate: {
-            validator: (email) => {
-                return /^[a-z\d]([a-z\d]+\.)*[a-z\d]+@([a-z\d]+\.)+[a-z]{2,4}$/.test(email);
-            },
-            message: `{VALUE} is not valid email`
-        }
+      type: String,
+      unique: true,
+      index: true,
+      lowercase: true,
+      trim: true,
+      validate: {
+        validator: (email) => {
+          return /^[a-z\d]([a-z\d]+\.)*[a-z\d]+@([a-z\d]+\.)+[a-z]{2,4}$/.test(
+            email
+          );
+        },
+        message: `{VALUE} is not valid email`,
+      },
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Name is required"],
     },
     password: {
-        type: String,
-        minlength: [6, "Password must be atleast 6 letters"]
+      type: String,
+      trim: true,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be atleast 6 letters"],
     },
     theme: {
-        type: String,
-        uppercase: true,
-        enum: {values: ["DARK", "LIGHT"], message: "{VALUE} is neither dark nor light"}
-    }
-},{timestamps: true});
+      type: String,
+      uppercase: true,
+      trim: true,
+      enum: {
+        values: ["DARK", "LIGHT"],
+        message: "{VALUE} is neither dark nor light",
+      },
+    },
+  },
+  { timestamps: true }
+);
 
-UserSchema.pre('validate', function(next){
-    this.email = this.email?.trim();
-    this.password = this.password?.trim();
-    next();
+UserSchema.pre("save", function (next) {
+  this.password = hashSync(this.password, genSaltSync(10));
+  next();
 });
 
-UserSchema.pre('save', function(next){
-    this.password = hashSync(this.password, genSaltSync(10));
-    next();
-})
-
-UserSchema.virtual('expenseLists', {
-    ref: "ExpenseList",
-    localField: "_id",
-    foreignField: "createdBy"
+UserSchema.virtual("expenseLists", {
+  ref: "ExpenseList",
+  localField: "_id",
+  foreignField: "createdBy",
 });
 
-UserSchema.set("toJSON", {virtuals: true});
-UserSchema.set("toObject", {virtuals: true});
+UserSchema.set("toJSON", { virtuals: true });
+UserSchema.set("toObject", { virtuals: true });
 
 UserSchema.statics.login = async function login(email, password) {
-    const user = await this.findOne({email});
-    if(!user)   throw new ApiError("User not found", 400, {email: "Email doesn't exists"});
-    if(!(await compare(password, user.password))){
-        throw new ApiError("User not found", 400, {password: `password doesn't match`});
-    }
-    return user;
-}
+  const user = await this.findOne({ email });
+  if (!user)
+    throw new ApiError("User not found", 400, {
+      email: "Email doesn't exists",
+    });
+  if (!(await compare(password, user.password))) {
+    throw new ApiError("User not found", 400, {
+      password: `password doesn't match`,
+    });
+  }
+  return user;
+};
 
 export const User = model("User", UserSchema);
