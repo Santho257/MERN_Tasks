d358b79840df4adb308dea413ef694262b856f4f
Santho257 <santhoshkumar02553@gmail.com>
2025-01-02 13:20:05 +0530

    Expense Tracker: Used the necessary icons

--- Week 3/ExpenseTracker/expense-frontend/src/components/ExpenseTable/EditExpense/EditExpense.jsx
+++ Week 3/ExpenseTracker/expense-frontend/src/components/ExpenseTable/EditExpense/EditExpense.jsx
@@ -1,64 +1,125 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react';
-import Input from '../../../ui/Input/Input';
-import { AuthContext } from '../../../contexts/AuthContext';
-import axios from 'axios';
-import Section from '../../../ui/Section/Section';
-import { BASE_URL } from '../../../constants';
-import Select, { Options } from '../../../ui/Select/Select';
-import Button from '../../../ui/Button/Button';
-import { useNavigate, useParams } from 'react-router-dom';
-import Form from '../../../ui/Form/Form';
-import Title from '../../../ui/Title/Title';
-import styles from './EditExpense.module.css'
-import { useFetch } from '../../../hooks/useFetch';
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import Input from "../../../ui/Input/Input";
+import { AuthContext } from "../../../contexts/AuthContext";
+import axios from "axios";
+import Section from "../../../ui/Section/Section";
+import { BASE_URL } from "../../../constants";
+import Select, { Options } from "../../../ui/Select/Select";
+import Button from "../../../ui/Button/Button";
+import { useNavigate, useParams } from "react-router-dom";
+import Form from "../../../ui/Form/Form";
+import Title from "../../../ui/Title/Title";
+import styles from "./EditExpense.module.css";
+import { useFetch } from "../../../hooks/useFetch";
+import Delete from "../../../ui/Icons/Delete";
+import Back from "../../../ui/Icons/Back";
 
 const EditExpense = () => {
     const navi = useNavigate();
     const { expId } = useParams();
     const { user } = useContext(AuthContext);
-    const initial = { source: "", category: "", amount: "", date: "" }
+    const initial = { source: "", category: "", amount: "", date: "" };
 
-    const [formdata, setFormdata, errors, setErrors] = useFetch(`${BASE_URL}/expenses/${expId}`, { Authorization: user.token }, initial);
+    const [formdata, setFormdata, errors, setErrors] = useFetch(
+        `${BASE_URL}/expenses/${expId}`,
+        { Authorization: user.token },
+        initial
+    );
 
     const category = {
         Category: "",
         Food: "food",
         Travel: "travel",
         Personal: "personal",
-        Family: "family"
-    }
+        Family: "family",
+    };
 
-    const handleFieldChange = useCallback((field, value) => {
-        setFormdata({ ...formdata, [field]: value })
-    }, [formdata]);
+    const handleFieldChange = useCallback(
+        (field, value) => {
+            setFormdata({ ...formdata, [field]: value });
+        },
+        [formdata]
+    );
+    const deleteExpense = useCallback(async (e) => {
+        try {
+            const result = await axios.delete(
+                `${BASE_URL}/expenses/${expId}`,
+                { headers: { Authorization: user.token } }
+            );
+            if (result.data.success) navi(-1);
+        } catch (error) {
+            setErrors(error?.response?.data?.errors || { message: error.message });
+        }
+    })
 
     const entryExpense = async (e) => {
         e.preventDefault();
         try {
-            const result = await axios.patch(`${BASE_URL}/expenses/${expId}`, formdata, { headers: { Authorization: user.token } });
-            if(result.data.success)
-                navi(-1);
-
+            const result = await axios.patch(
+                `${BASE_URL}/expenses/${expId}`,
+                formdata,
+                { headers: { Authorization: user.token } }
+            );
+            if (result.data.success) navi(-1);
         } catch (error) {
-            setErrors(error?.response?.data?.errors || { message: error.message })
+            setErrors(error?.response?.data?.errors || { message: error.message });
         }
-    }
+    };
     return (
         <Section className={styles.container}>
             <Section>
-                <Title>Edit Expense</Title>
                 <Form onSubmit={entryExpense}>
-                    <Input type="text" name="source" id="source" label="source" value={formdata.source} onChange={(e) => handleFieldChange("source", e.target.value)} error={errors.source} placeholder="Source" />
-                    <Select name="category" id="category" label="category" onChange={(e) => handleFieldChange("category", e.target.value)} error={errors?.category} value={formdata.category.toLowerCase()} placeholder="Category">
+                    <Title>
+                        <span onClick={() => navi(-1)}><Back /></span>
+                        Edit Expense
+                        <span onClick={deleteExpense}><Delete /></span>
+                    </Title>
+                    <Input
+                        type="text"
+                        name="source"
+                        id="source"
+                        label="source"
+                        value={formdata.source}
+                        onChange={(e) => handleFieldChange("source", e.target.value)}
+                        error={errors.source}
+                        placeholder="Source"
+                    />
+                    <Select
+                        name="category"
+                        id="category"
+                        label="category"
+                        onChange={(e) => handleFieldChange("category", e.target.value)}
+                        error={errors?.category}
+                        value={formdata.category.toLowerCase()}
+                        placeholder="Category"
+                    >
                         <Options options={category} />
                     </Select>
-                    <Input type="date" name="date" id="date" label="Date" value={formdata?.date?.slice(0, 10)} onChange={(e) => handleFieldChange("date", e.target.value)} error={errors.date} placeholder="Date" />
-                    <Input type="number" name="amount" id="amount" label="Amount" value={formdata.amount} onChange={(e) => handleFieldChange("amount", e.target.value)} error={errors.amount} placeholder="Amount" />
+                    <Input
+                        type="date"
+                        name="date"
+                        id="date"
+                        label="Date"
+                        value={formdata?.date?.slice(0, 10)}
+                        onChange={(e) => handleFieldChange("date", e.target.value)}
+                        error={errors.date}
+                        placeholder="Date"
+                    />
+                    <Input
+                        type="number"
+                        name="amount"
+                        id="amount"
+                        label="Amount"
+                        value={formdata.amount}
+                        onChange={(e) => handleFieldChange("amount", e.target.value)}
+                        error={errors.amount}
+                        placeholder="Amount"
+                    />
                     <Button type="submit">Edit Expense</Button>
                 </Form>
             </Section>
         </Section>
-    )
-}
+    );
+};
 
-export default React.memo(EditExpense)
+export default React.memo(EditExpense);
--- Week 3/ExpenseTracker/expense-frontend/src/components/Login/Login.jsx
+++ Week 3/ExpenseTracker/expense-frontend/src/components/Login/Login.jsx
@@ -1,42 +1,45 @@
-import React, { useCallback, useContext, useState } from 'react'
-import Section from '../../ui/Section/Section'
-import Input from '../../ui/Input/Input'
-import styles from './login.module.css'
-import Button from '../../ui/Button/Button'
-import axios from 'axios'
-import { BASE_URL } from '../../constants'
-import { AuthContext } from '../../contexts/AuthContext'
-import { useNavigate } from 'react-router-dom'
-import Title from '../../ui/Title/Title'
-import Form from '../../ui/Form/Form'
+import React, { useCallback, useContext, useState } from "react";
+import Section from "../../ui/Section/Section";
+import Input from "../../ui/Input/Input";
+import styles from "./login.module.css";
+import Button from "../../ui/Button/Button";
+import axios from "axios";
+import { BASE_URL } from "../../constants";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import Title from "../../ui/Title/Title";
+import Form from "../../ui/Form/Form";
+import EyeClose from "../../ui/Icons/EyeClose";
+import EyeOpen from "../../ui/Icons/EyeOpen";
 
 const Login = ({ login }) => {
     const { logIn } = useContext(AuthContext);
+    const [visiblePass, setVisiblePass] = useState(false);
     const navi = useNavigate();
-    const [errors, setErrors] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState({ email: "", password: "", name: "" });
     const [formData, setFormData] = useState({
-        email: "", password: "", rePassword: ""
+        email: "",
+        password: "",
+        name: "",
     });
     const handleChange = useCallback((field, value) => {
-        setFormData({ ...formData, [field]: value })
+        setFormData({ ...formData, [field]: value });
     });
     const handleLogin = async (e) => {
-        setErrors({})
+        setErrors({});
         e.preventDefault();
         let result;
         try {
             if (login) {
                 result = await axios.post(`${BASE_URL}/auth/login`, formData);
-            }
-            else {
+            } else {
                 result = await axios.post(`${BASE_URL}/auth/signup`, formData);
             }
             sessionStorage.setItem("user", result.data.data.token);
             logIn(result.data.data.token);
-            navi('/expenses')
-        }
-        catch (err) {
-            setErrors(err.response.data.errors)
+            navi("/expenses");
+        } catch (err) {
+            setErrors(err.response.data.errors);
         }
     };
     return (
@@ -44,14 +47,46 @@ const Login = ({ login }) => {
             <Section style={{ width: "540px" }}>
                 <Title>{login ? "Login" : "Create Account"}</Title>
                 <Form onSubmit={handleLogin}>
-                    <Input type="email" name="email" id="email" value={formData.email} onChange={(e) => handleChange("email", e.target.value)} placeholder="email" error={errors.email} />
-                    <Input type="password" name="password" id="password" value={formData.password} onChange={(e) => handleChange("password", e.target.value)} placeholder="password" error={errors.password} />
-                    {/* {!login && <Input type="password" name="Re-Password" id="Re-Password" value={formData.rePassword} onChange={(e) => handleChange("rePassword", e.target.value)} placeholder="Reenter Password"/>} */}
+                    <Input
+                        type="email"
+                        name="email"
+                        id="email"
+                        value={formData.email}
+                        onChange={(e) => handleChange("email", e.target.value)}
+                        placeholder="email"
+                        error={errors.email}
+                    />
+                    {!login && (
+                        <>
+                            <Input
+                                type="text"
+                                name="name"
+                                id="name"
+                                value={formData.name}
+                                onChange={(e) => handleChange("name", e.target.value)}
+                                placeholder="name"
+                            />
+                        </>
+                    )}
+                    <Section style={{ padding: "0" }}>
+                        <Input
+                            type={visiblePass ? "text" : "password"}
+                            name="password"
+                            id="password"
+                            value={formData.password}
+                            onChange={(e) => handleChange("password", e.target.value)}
+                            placeholder="password"
+                            error={errors.password}
+                        />
+                        <span className={styles.visiblePass} onClick={() => setVisiblePass((v) => !v)}>
+                            {visiblePass ? <EyeOpen /> : <EyeClose />}
+                        </span>
+                    </Section>
                     <Button type="submit">{login ? "LOGIN" : "SIGN UP"}</Button>
                 </Form>
             </Section>
         </Section>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
