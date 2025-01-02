e8b3de85fe2895ef778761638d2dc65f4c10a326
Santho257 <santhoshkumar02553@gmail.com>
2025-01-02 13:11:20 +0530

    Expense Tracker: Navigate default route to expenses

--- Week 3/ExpenseTracker/expense-frontend/src/components/Routes/Router.jsx
+++ Week 3/ExpenseTracker/expense-frontend/src/components/Routes/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Login from '../Login/Login'
 import RequireAuth from '../Authentication/RequireAuth'
 import ExpenseListArea from '../ExpenseListArea/ExpenseListArea'
@@ -9,12 +9,13 @@ import EditExpense from '../ExpenseTable/EditExpense/EditExpense'
 const Router = () => {
     return (
         <Routes>
-            <Route exact path='/login' element={<Login login/>}/>
-            <Route exact path='/signup' element={<Login/>}/>
+            <Route exact path='/' element={<Navigate to='/expenses' />} />
+            <Route exact path='/login' element={<Login login />} />
+            <Route exact path='/signup' element={<Login />} />
             <Route element={<RequireAuth />}>
-                <Route exact path='/expenses' element={<ExpenseListArea />}/>
-                <Route exact path='/expenses/:explistId' element={<ExpenseTable />}/>
-                <Route exact path='/expenses/:explistId/:expId' element={<EditExpense />}/>
+                <Route exact path='/expenses' element={<ExpenseListArea />} />
+                <Route exact path='/expenses/:explistId' element={<ExpenseTable />} />
+                <Route exact path='/expenses/:explistId/:expId' element={<EditExpense />} />
             </Route>
         </Routes>
     )
