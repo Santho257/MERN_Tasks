7f666828c9951263e9f154ad3e91413d6b9e6c8b
Santho257 <santhoshkumar02553@gmail.com>
2025-01-02 13:09:51 +0530

    Expense tracker: Showing user name on header

--- Week 3/ExpenseTracker/expense-frontend/src/components/Header/Header.jsx
+++ Week 3/ExpenseTracker/expense-frontend/src/components/Header/Header.jsx
@@ -1,26 +1,28 @@
 import styles from './header.module.css'
-import {Link, Outlet} from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import ThemeSetter from './ThemeSetter/ThemeSetter'
 import { useContext } from 'react'
 import { AuthContext } from '../../contexts/AuthContext'
+import { jwtDecode } from 'jwt-decode'
 
 const Header = () => {
-	const {user, logOut} = useContext(AuthContext);
+	const { user, logOut } = useContext(AuthContext);
 	return (
 		<>
-		<header className={styles.header}>
-			<h2>Expense Tracker</h2>
-			<nav>
-				<ul>
-					{user.token && <li><Link to="/expenses">Expenses</Link></li>}
-					{!user.token && <li><Link to="/signup">Signup</Link></li>}
-					{!user.token && <li><Link to="/login">Login</Link></li>}
-					{user.token && <li onClick={logOut}>Logout</li>}
-					<li><ThemeSetter/></li>
-				</ul>
-			</nav>
-		</header>
-		<Outlet/>
+			<header className={styles.header}>
+				<h2>Expense Tracker</h2>
+				<nav>
+					<ul>
+						{user.token && <li>{jwtDecode(user.token).sub}</li>}
+						{user.token && <li><Link to="/expenses">Expenses</Link></li>}
+						{!user.token && <li><Link to="/signup">Signup</Link></li>}
+						{!user.token && <li><Link to="/login">Login</Link></li>}
+						{user.token && <li onClick={logOut}>Logout</li>}
+						<li><ThemeSetter /></li>
+					</ul>
+				</nav>
+			</header>
+			<Outlet />
 		</>
 	)
 }
