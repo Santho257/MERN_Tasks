f004ac64b69c9c4129238af7d285fb43bb326f83
Santho257 <santhoshkumar02553@gmail.com>
2025-01-15 12:33:14 +0530

    Implemented JWT Service

---
+++ Blog-App/blog-backend/src/services/jwt.service.js
@@ -0,0 +1,28 @@
+import { decode, sign, verify } from "jsonwebtoken"
+import { JWT_SECRET } from "../constants.js"
+import ApiError from "../utils/ApiError.js";
+
+export const generateToken = (payload) => {
+    return sign(payload, JWT_SECRET, { expiresIn: "10h" });
+}
+
+const extractToken = authorization => {
+    if(authorization.startsWith("Bearer "))
+        return authorization.slice(7);
+    throw new ApiError(400, "No Bearer token", {token: "Invalid Authorization header"});
+}
+
+export const verifyToken = authorization => {
+    const token = extractToken(authorization);
+    try {
+        return verify(token, JWT_SECRET);
+    } catch (error) {
+        throw new ApiError(400, "Invalid Token", { token: "Token is invalid" });
+    }
+}
+
+export const getIdFromToken = authorization => {
+    const token = extractToken(authorization)
+    if(verifyToken(token))
+        return decode(token).id;
+}
