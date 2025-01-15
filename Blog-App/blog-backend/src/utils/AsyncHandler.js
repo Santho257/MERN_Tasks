8b9869b50e6d52b2dec6cc8ff39918bebfd3c326
Santho257 <santhoshkumar02553@gmail.com>
2025-01-15 11:36:26 +0530

    Handled ApiError

--- Blog-App/blog-backend/src/utils/AsyncHandler.js
+++ Blog-App/blog-backend/src/utils/AsyncHandler.js
@@ -1,6 +1,11 @@
-const asyncHandler = func => (req, res, next) => {
+import ApiError from "./ApiError.js"
+
+export const asyncHandler = func => (req, res, next) => {
     Promise.resolve(func(req, res, next))
         .catch(error => {
+            if(error instanceof ApiError){
+                res.status(error.status).send(error);
+            }
             res.status(500).send(new ApiError(500, "Unknown Error"))
         })
 }
