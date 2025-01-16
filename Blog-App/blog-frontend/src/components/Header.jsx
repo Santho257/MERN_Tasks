fe3f31f68813605002344361fe285d0a6c7df8cc
Santho257 <santhoshkumar02553@gmail.com>
2025-01-16 13:18:45 +0530

    Created Header Component

---
+++ Blog-App/blog-frontend/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h3 className='logo'>Own Thoughts</h3>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Blogs</li>
+                <li>Write</li>
+                <li>Sign-in</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
