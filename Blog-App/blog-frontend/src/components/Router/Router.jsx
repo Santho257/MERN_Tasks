6d3a329f7da826c3e0474a97eb1b044cb5d90fcd
Santho257 <santhoshkumar02553@gmail.com>
2025-01-17 12:39:12 +0530

    Updated Routes for login

--- Blog-App/blog-frontend/src/components/Router/Router.jsx
+++ Blog-App/blog-frontend/src/components/Router/Router.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import AllBlogs from '../AllBlogs/AllBlogs'
 import RequireAuth from '../RequireAuth/RequireAuth'
+import Login from '../Login/Login'
 
 const Router = () => {
 	return (
 		<Routes>
 			<Route exact path='/' element={<Navigate to={"/blogs"} />} />
 			<Route exact path='/blogs' element={<AllBlogs />} />
+			<Route exact path='/signin' element={<Login log={true} />} />
+			<Route exact path='/signup' element={<Login />} />
 			<Route element={<RequireAuth />}>
 				<Route exact path='/blogs/:id' element={<p>Reading blog</p>} />
 			</Route>
