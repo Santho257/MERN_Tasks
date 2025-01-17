bc590607eab7ffdc285be7e915caf43681999a7a
Santho257 <santhoshkumar02553@gmail.com>
2025-01-17 17:13:53 +0530

    Removed unwanted imports

--- Blog-App/blog-frontend/src/App.jsx
+++ Blog-App/blog-frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
 import './App.css'
-import BlogArea from './components/BlogArea'
 import Router from './components/Router/Router'
 
 const App = () => {
