87a53704065dd7918520506384a651495cb769ed
Santho257 <santhoshkumar02553@gmail.com>
2025-01-17 12:37:03 +0530

    Excluded get all from authentication

--- Blog-App/blog-backend/src/routes/blogs.route.js
+++ Blog-App/blog-backend/src/routes/blogs.route.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { createBlog, deleteBlog, getByAuthors, getById, getPublished, updateBlog } from "../controllers/blogs.controller.js";
+import { requireAuth } from "../middlewares/requireauth.middleware.js";
 
 const BlogRouter = Router();
 
-BlogRouter.route("/").post(createBlog);
 BlogRouter.route("/").get(getPublished);
-BlogRouter.route("/:id").get(getById);
-BlogRouter.route("/:id").patch(updateBlog);
-BlogRouter.route("/authors/:authorId").get(getByAuthors);
-BlogRouter.route("/:id").delete(deleteBlog);
+BlogRouter.route("/:id").get(requireAuth, getById);
+BlogRouter.route("/authors/:authorId").get(requireAuth, getByAuthors);
+BlogRouter.route("/").post(requireAuth, createBlog);
+BlogRouter.route("/:id").patch(requireAuth, updateBlog);
+BlogRouter.route("/:id").delete(requireAuth, deleteBlog);
 
 export default BlogRouter;
