83403664da8e18d5c1e59d470c9f65a603bffc9e
Santho257 <santhoshkumar02553@gmail.com>
2025-01-17 17:12:30 +0530

    Created NewBlog component

---
+++ Blog-App/blog-frontend/src/components/NewBlog/NewBlog.jsx
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import React, { useContext, useEffect } from 'react'
+import { BASE_URL } from '../../constants';
+import { AuthContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+const NewBlog = () => {
+    const {user} = useContext(AuthContext);
+    const navi = useNavigate();
+    useEffect(() => {
+        createBlog();
+    });
+    const createBlog = async () => {
+        try {
+            const response = await axios.post(`${BASE_URL}/blogs`, {
+                title: "", content: [{
+                    type: "Para",
+                    value: ""
+                }]
+            }, {headers:{Authorization: `Bearer ${user.token}`}});
+            if(response.data?.data?.id){
+                navi(`/blogs/e/${response.data?.data?.id}`)
+            }
+        } catch (error) {
+            console.log("Errors");
+        }
+    };
+    return (
+        <></>
+    )
+}
+
+export default NewBlog
