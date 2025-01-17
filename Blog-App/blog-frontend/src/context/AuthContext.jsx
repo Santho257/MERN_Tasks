650e0d3113f9342c0c425986816e326f4f587792
Santho257 <santhoshkumar02553@gmail.com>
2025-01-17 14:19:03 +0530

    Fixed logout bug

--- Blog-App/blog-frontend/src/context/AuthContext.jsx
+++ Blog-App/blog-frontend/src/context/AuthContext.jsx
@@ -1,28 +1,28 @@
 import { createContext, useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const initial = {
-    token: JSON.parse(sessionStorage.getItem("blog-user"))?.token || ""
-}
 
 const AuthContext = createContext(initial);
 
 const AuthContextProvider = ({ children }) => {
+    const initial = {
+        token: JSON.parse(sessionStorage.getItem("blog-user"))?.token || ""
+    }
     const navi = useNavigate();
     const [user, setUser] = useState(initial);
 
     const login = useCallback(token => {
         sessionStorage.setItem("blog-user", JSON.stringify({ token }));
         setUser({ ...user, token })
-    }, []);
+    }, [user]);
 
     const logout = useCallback(() => {
         sessionStorage.setItem("blog-user", JSON.stringify({ token: "" }));
         setUser({ ...initial });
-        navi("/signin")
-    }, []);
+        navi("/blogs")
+    }, [user]);
 
-    return <AuthContext.Provider value={{user, login, logout}}>{children}</AuthContext.Provider>
+    return <AuthContext.Provider value={{ user, login, logout }}>{children}</AuthContext.Provider>
 }
 
-export {AuthContext, AuthContextProvider};
+export { AuthContext, AuthContextProvider };
