689f82cccaadd923353c5c4ce94f3d4eced94c76
Santho257 <santhoshkumar02553@gmail.com>
2025-01-17 09:13:37 +0530

    Changed asyncHandler to fix bug

--- Blog-App/blog-backend/src/utils/AsyncHandler.js
+++ Blog-App/blog-backend/src/utils/AsyncHandler.js
@@ -12,14 +12,15 @@ export const asyncHandler = func => (req, res, next) => {
                 res.status(400).send(new ApiError(400, error.name, errors, error.stackTraceLimit));
                 return;
             }
-            if(error instanceof ApiError){
+            else if(error instanceof ApiError){
                 res.status(error.status).send(error);
+                return;
             }
-            if(error instanceof MongooseError && error.kind == "ObjectId"){
+            else if(error instanceof MongooseError && error.kind == "ObjectId"){
                 res.status(400).send(new ApiError(400, error.message, {id: `${error.value} is not ObjectID`}, error.stackTraceLimit));
                 return;
             }
-            if(error.code == 11000){
+            else if(error.code == 11000){
                 const errors = {};
                 for(let err in error.keyValue){
                     errors[err] = `${error.keyValue[err]} already exists!`;
@@ -27,6 +28,7 @@ export const asyncHandler = func => (req, res, next) => {
                 res.status(400).send(new ApiError(400, error.message, errors, error.stackTraceLimit));
                 return;
             }
-            res.status(500).send(new ApiError(500, error.message ?? "Unknown Error", error, error.stackTraceLimit))
+            else
+                res.status(500).send(new ApiError(500, error.message ?? "Unknown Error", error.message, error.stackTraceLimit))
         })
 }
