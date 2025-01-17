732465dd7955935be85b5138e213a7ce4192b6d6
Santho257 <santhoshkumar02553@gmail.com>
2025-01-17 09:18:16 +0530

    Removed unnecessary import

--- Blog-App/blog-frontend/src/App.jsx
+++ Blog-App/blog-frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
 import './App.css'
-import Input from './components/Input'
 import BlogArea from './components/BlogArea'
 
 const App = () => {
