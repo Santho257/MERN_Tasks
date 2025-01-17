e84e88f45246edd424d182a6f9485660f391c46c
Santho257 <santhoshkumar02553@gmail.com>
2025-01-17 10:24:54 +0530

    Modified Header

--- Blog-App/blog-frontend/src/components/Header.jsx
+++ Blog-App/blog-frontend/src/components/Header.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import Button from '../ui/Button/Button'
+import { AuthContext } from '../context/AuthContext'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const {user, logout} = useContext(AuthContext);
+  useEffect(() => {
+    console.log("Header");
+    
+  },[])
   return (
     <header>
         <h3 className='logo'>Own Thoughts</h3>
         <nav>
             <ul>
-                <li>Home</li>
-                <li>Blogs</li>
-                <li>Write</li>
-                <li>Sign-in</li>
+                <li><Link to="/blogs">Blogs</Link></li>
+                <li><Link to="/new">Write</Link></li>
+                {!user?.token && <li><Link to="/signin"><Button>Sign In</Button></Link></li>}
+                {user?.token && <li><Button onClick={logout}>logout</Button></li>}
             </ul>
         </nav>
     </header>
