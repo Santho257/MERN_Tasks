b9cbb95c4c0b7bdf7a43bfee260a87cfeb57d49a
Santho257 <santhoshkumar02553@gmail.com>
2025-01-27 14:36:42 +0530

    Add auth route to server

--- Task Manager/task-backend/src/app.js
+++ Task Manager/task-backend/src/app.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import cors from 'cors';
-import { ALLOWED_ORIGINS } from './constants.js';
+import { ALLOWED_ORIGINS, BASE_URL } from './constants.js';
 import errorHandler from './middlewares/errorhandle.middleware.js';
+import AuthRouter from './routes/auth.route.js';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({origin: ALLOWED_ORIGINS}));
+
+app.use(`${BASE_URL}/auth`, AuthRouter);
 
 app.use(errorHandler);
 export default app;
