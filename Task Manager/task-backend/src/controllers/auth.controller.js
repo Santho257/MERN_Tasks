2d0dccbb6554a07b49816c298e7c6a7e88a58009
Santho257 <santhoshkumar02553@gmail.com>
2025-01-27 15:27:48 +0530

    Send as ApiResponse

--- Task Manager/task-backend/src/controllers/auth.controller.js
+++ Task Manager/task-backend/src/controllers/auth.controller.js
@@ -1,12 +1,13 @@
 import { User } from "../models/user.model.js";
 import { generateToken } from "../services/jwt.service.js";
+import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asynHandler.js";
 
 const signup = asyncHandler(async (req, res, next) => {
     try {
         const user = await User.create(req.body);
         const token = await generateToken(user);
-        res.status(201).send({ token });
+        res.status(201).send(new ApiResponse(201, { token }, "User created successfully"));
     } catch (error) {
         next(error);
     }
@@ -17,7 +18,7 @@ const login = asyncHandler(async (req, res, next) => {
         const { email, password } = req.body;
         const user = await User.login(email, password);
         const token = await generateToken(user);
-        res.status(201).send({ token });
+        res.status(201).send(new ApiResponse(200, { token }, "User logged in successfully"));
     } catch (error) {
         next(error);
     }
