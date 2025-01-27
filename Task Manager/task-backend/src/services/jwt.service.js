f884727e32205a141637a0e2466212d681c348d5
Santho257 <santhoshkumar02553@gmail.com>
2025-01-27 14:29:25 +0530

    Create JWT service

---
+++ Task Manager/task-backend/src/services/jwt.service.js
@@ -0,0 +1,30 @@
+import { decode } from "jsonwebtoken";
+import { JWT_EXPIRES_IN, JWT_SECRET } from "../constants.js";
+
+export const generateToken = (user) => {
+    return jwt.sign({ id: user._id }, JWT_SECRET, {
+        expiresIn: JWT_EXPIRES_IN
+    });
+}
+
+export const extractToken = (req) => {
+    if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+        throw new ApiError(400, { token: "Invalid JWT Token" }, "Invalid Authorization Header");
+    }
+    return req.headers.authorization.split(' ')[1];
+}
+
+export const verifyToken = (req) => {
+    const token = extractToken(req);
+    try {
+        return verify(token, JWT_SECRET);
+    } catch (error) {
+        throw new ApiError(400, "Invalid Token", { token: "Token is invalid" });
+    }
+}
+
+export const getIdFromToken = (req) => {
+    if(verifyToken(req)){
+        return decode(extractToken(req)).id;
+    }
+}
