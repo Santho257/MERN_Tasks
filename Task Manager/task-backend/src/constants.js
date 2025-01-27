fc8c28f2030c6a1c83e6ceb9945b48d258e8cb3f
Santho257 <santhoshkumar02553@gmail.com>
2025-01-27 14:29:39 +0530

    Exporting JWT expires time

--- Task Manager/task-backend/src/constants.js
+++ Task Manager/task-backend/src/constants.js
@@ -6,4 +6,5 @@ export const MONGO_URL = process.env.MONGO_URL;
 export const MONGO_DB = process.env.MONGO_DB;
 export const PORT = process.env.PORT || 3000;
 export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 export const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS;
