531952c62b1778b7aaf92c0ea558d450c5893424
Santho257 <santhoshkumar02553@gmail.com>
2025-01-27 14:04:45 +0530

    Add errorHandler in app

--- Task Manager/task-backend/src/app.js
+++ Task Manager/task-backend/src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import { ALLOWED_ORIGINS } from './constants.js';
+import errorHandler from './middlewares/errorhandle.middleware.js';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({origin: ALLOWED_ORIGINS}));
 
+app.use(errorHandler);
 export default app;
